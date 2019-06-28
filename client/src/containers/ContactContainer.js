f37552b1727517bfed14f254924741ca5f52e2f9
dnj547 <dnj547@live.com>
2019-06-28 11:21:14 -0400

    Add content to projects page, start on contact page

--- client/src/containers/ContactContainer.js
+++ client/src/containers/ContactContainer.js
@@ -3,8 +3,32 @@ import React, { Component } from 'react';
 class ContactContainer extends Component {
   render() {
     return (
-      <div className="contact-container ui container">
-        Contact Container
+      <div className="contact-container">
+        <div className="contact-form">
+          <h1>Contact Me</h1>
+          <form>
+              <input
+                type="text"
+                name="name"
+                placeholder="Bill Gates"
+                size="50"/>
+              <br/>
+              <input
+                type="text"
+                name="email"
+                placeholder="realbillgates@microsoft.com"
+                size="50"/>
+              <br/>
+              <textarea
+                name="message"
+                placeholder="Wow Danielle, I love your site. Please make one for me! Here’s 50k in Bitcoin! [bitcoin link redacted]"
+                rows="20"
+                cols="48"
+                wrap="soft"/>
+              <br/>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     );
   }
--- client/src/containers/ProjectsContainer.js
+++ client/src/containers/ProjectsContainer.js
@@ -3,9 +3,30 @@ import data from '../data/project_data.json';
 import Project from '../components/Project';
 
 class ProjectsContainer extends Component {
+
+  state = {
+    currentProject: 1
+  }
+
+  nextProject = () => {
+    if (this.state.currentProject < data.length) {
+      this.setState({
+        currentProject: this.state.currentProject+1
+      })
+    }
+  }
+
+  previousProject = () => {
+    if (this.state.currentProject > 1) {
+      this.setState({
+        currentProject: this.state.currentProject-1
+      })
+    }
+  }
+
   projectComponents = () => {
     return data.map(project=>{
-      return <Project key={project.id} project={project} />
+      return <Project key={project.id} project={project} currentProject={this.state.currentProject} nextProject={this.nextProject} previousProject={this.previousProject}/>
     })
   }
   render() {
