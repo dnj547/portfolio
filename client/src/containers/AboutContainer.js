af3033b5b2fb7c739973d08695c471bc1313834f
dnj547 <dnj547@live.com>
2019-07-11 11:01:28 -0400

    Almost done

--- client/src/containers/AboutContainer.js
+++ client/src/containers/AboutContainer.js
@@ -6,14 +6,24 @@ class AboutContainer extends Component {
       <div className="about-container">
         <div className="about-container-stuff">
           <div className="about-me-card">
-            <img alt="me" src="images/me.jpg"/>
+            <img alt="me" src="images/me.jpg"/><br/><br/>
+            <a href="https://github.com/dnj547">
+              <button className="about-me-button">GitHub</button>
+            </a>
+            <a href="https://medium.com/@devdanielle">
+              <button className="about-me-button">Medium</button>
+            </a>
+            <a href="https://www.linkedin.com/in/daniellejasper/">
+              <button className="about-me-button">LinkedIn</button>
+            </a>
+            <a href="https://docs.google.com/presentation/d/1LaTpyhd_-QaJcM2NbAiQJMTuIldh-axyvpzFLnIlpmc/edit?usp=sharing">
+              <button className="about-me-button">Resumé</button>
+            </a>
           </div>
           <div className="about-me-text">
-            <h1>About Me</h1>
-            <h2>Hi, I'm Danielle.</h2>
-            <p>I'm a full-stack software developer who just graduated from Flatiron School! (July 2019)</p>
-            <h3>Links:</h3>
-            <a href="https://github.com/dnj547">GitHub</a> | <a href="https://medium.com/@devdanielle">Medium</a> | <a href="https://www.linkedin.com/in/daniellejasper/">LinkedIn</a>
+            <h1>About Me</h1><br/>
+            <p>Hi, I'm Danielle. I'm a full-stack software developer who just graduated from Flatiron School in NYC!</p>
+            <h2></h2>
           </div>
         </div>
       </div>
